a5da968e9cf6c0efea253ed755eb5c24c606885b
feiyang.wang <feiyang.wang@estudiantat.upc.edu>
2024-03-11 12:58:12 +0100

    estructura básica

--- src/Kenken.java
+++ src/Kenken.java
@@ -1,6 +1,23 @@
+package src;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Kenken {
     private int size;
-    private
-    private TypeDificult dificult;
+    private HashSet<Character> operations;
+    private ArrayList<KenkenCage> cages;
+    public TypeDificult dificult;
+
+    Kenken(){
+        size = 0;
+
+    }
+
+    Kenken(int size, HashSet<Character> operations, TypeDificult dificult){
+        this.size = size;
+        this.operations = operations;
+        this.dificult = dificult;
+
+    }
+
 }
---
+++ src/KenkenCage.java
@@ -0,0 +1,9 @@
+package src;
+
+import java.util.ArrayList;
+
+public class KenkenCage {
+    private ArrayList<KenkenCell> cells;
+    private Character operation;
+    private int result;
+}

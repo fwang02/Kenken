c8e999e97689f69eb7df83f997f150f26f5b7abf
javier.parcerisas <javier.parcerisas@estudiantat.upc.edu>
2024-03-14 09:02:13 +0100

    RandomMatrix

--- src/Kenken.java
+++ src/Kenken.java
@@ -12,7 +12,6 @@ public class Kenken {
         size = 0;
         cages = new ArrayList<KenkenCage>();
         operations = new HashSet<TypeOperation>();
-
     }
 
     Kenken(int size, HashSet<TypeOperation> operations, TypeDificult dificult){

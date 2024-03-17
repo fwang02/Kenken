02639c5564c7c15a7f9d537da1048a4c83f434bc
javier.parcerisas <javier.parcerisas@estudiantat.upc.edu>
2024-03-17 13:58:58 +0100

    kenken mod @javi

--- FONTS/src/Kenken.java
+++ FONTS/src/Kenken.java
@@ -6,6 +6,7 @@ public class Kenken {
     private final int size;
     private final HashSet<TypeOperation> operations;
     private final ArrayList<KenkenCage> cages;
+    private ArrayList<KenkenCell> cells;
     private final TypeDificult dificult;
     private String name;
 
@@ -13,8 +14,9 @@ public class Kenken {
     Kenken(){
         this.dificult = TypeDificult.MEDIUM;
         size = 0;
-        cages = new ArrayList<KenkenCage>();
         operations = new HashSet<TypeOperation>();
+        cages = new ArrayList<KenkenCage>();
+        iniCells();
     }
 
     Kenken(int size, HashSet<TypeOperation> operations, TypeDificult dificult){
@@ -22,6 +24,7 @@ public class Kenken {
         this.operations = operations;
         this.dificult = dificult;
         cages = new ArrayList<KenkenCage>();
+        iniCells();
     }
 
     Kenken(int size, HashSet<TypeOperation> operations, TypeDificult dificult, String name){
@@ -29,25 +32,52 @@ public class Kenken {
         this.operations = operations;
         this.dificult = dificult;
         cages = new ArrayList<KenkenCage>();
+        iniCells();
         this.name = name;
     }
-    public void generate() {
+
+
+    public int getWidth() {
+        return size;
+    }
+
+
+    public int getHeight() {
+        return size;
+    }
+
+
+    public KenkenCell getCell(int i, int j) {
+        return cells.get(i*getWidth()+j);
+    }
+
+    
+    private void iniCells() {
+        cells = new ArrayList<KenkenCell>();
+        KenkenCell tmp = null;
         for(int i = 0; i < size; ++i) {
             for(int j = 0; j < size; ++j) {
-
+                tmp = new KenkenCell(i,j,-1, false);
+                cells.add(tmp);
             }
         }
+    }
+
+    public boolean rowCheck(int row, int val) {
+        for(int col = 0; col < getWidth(); ++col) {
+            if(getCell(row, col).getValue() == val) return false;
+        }
+        return true;
+    }
+
+    public boolean colCheck(int col, int val) {
+        for(int row = 0; row < getHeight(); ++row) {
+            if(getCell(row, col).getValue() == val) return false;
+        }
+        return true;
     }
 
     /*public boolean isValid() {
 
     }*/
-
-//    public boolean rowCheck() {
-//
-//    }
-
-//    public boolean columnCheck() {
-//
-//    }
 }
--- FONTS/src/KenkenCell.java
+++ FONTS/src/KenkenCell.java
@@ -13,6 +13,10 @@ public class KenkenCell {
         this.locked = locked;
     }
 
+    public void setValue(int val) {  //@javi: feiyang he añadido esto para poder ir poniendo los valores a las cells :D cuando los modifiquemos.
+        valor = val;                //tipo al momento de inicializarlos les paso el valor = -1. Cuando genere la matriz correctamente podre usar esta
+    }                               //funcion para modificar el valor de la celda.
+
     public int getPosX() {
         return posX;
     }

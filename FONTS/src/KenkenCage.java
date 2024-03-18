29f5db73d77df501dbf867e9398a62e33659d19e
feiyang.wang <feiyang.wang@estudiantat.upc.edu>
2024-03-18 14:50:22 +0100

    KenkenCage update

--- FONTS/src/KenkenCage.java
+++ FONTS/src/KenkenCage.java
@@ -1,6 +1,7 @@
 package FONTS.src;
 import java.util.ArrayList;
-import java.lang.Math;
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
 
 public class KenkenCage {
     private final ArrayList<KenkenCell> cells;
@@ -21,7 +22,14 @@ public class KenkenCage {
         this.operation = operation;
         this.result = result;
         allLocked = false;
+    }
 
+    //Constructora para creado por fichero
+    KenkenCage(TypeOperation operation, int result, ArrayList<KenkenCell> cells) {
+        this.cells = cells;
+        this.operation = operation;
+        this.result = result;
+        allLocked = false;
     }
 
     public boolean isCageValid() {
@@ -35,7 +43,7 @@ public class KenkenCage {
             if(allLocked) return result == sum;
             else return result > sum;
         } else if (operation == TypeOperation.SUB) {
-            if(allLocked) return Math.abs(cells.get(0).getValue() - cells.get(1).getValue()) == result;
+            if(allLocked) return abs(cells.get(0).getValue() - cells.get(1).getValue()) == result;
         } else if (operation == TypeOperation.MULT) {
             int mult = 1;
             for (KenkenCell cell : cells) {
@@ -51,11 +59,30 @@ public class KenkenCage {
                 else return cells.get(1).getValue() / cells.get(0).getValue() == result;
             }
         } else if (operation == TypeOperation.MOD) {
-
+            if(allLocked) {
+                return cells.get(0).getValue() % cells.get(1).getValue() == result || cells.get(1).getValue() % cells.get(0).getValue() == result;
+            }
         } else if (operation == TypeOperation.POW) {
-
-
+            if(allLocked) {
+                return pow(cells.get(0).getValue(),cells.get(1).getValue()) == result || pow(cells.get(1).getValue(),cells.get(0).getValue()) == result;
+            }
         }
         return true;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public boolean isAllLocked() {
+        return allLocked;
+    }
+
+    public TypeOperation getOperation() {
+        return operation;
+    }
+
+    public ArrayList<KenkenCell> getCells() {
+        return cells;
     }
 }
